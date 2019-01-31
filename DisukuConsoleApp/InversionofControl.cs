dae2afadd3eb9ca884bad80f360e5677cf7cee1a
Joel Parkinson <joelparkinson@live.co.uk>
2019-01-31 11:12:28 +0000

    Added Console APp with IOC

---
+++ DisukuConsoleApp/InversionofControl.cs
@@ -0,0 +1,34 @@
+﻿using DisukuBot.DisukuCore;
+using DisukuBot.DisukuDiscord;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DisukuConsoleApp
+{
+    public static class InversionOfControl
+    {
+        private static ServiceProvider provider;
+
+        public static ServiceProvider Provider
+        {
+            get
+            {
+                return GetOrInitProvider();
+            }
+        }
+
+        private static ServiceProvider GetOrInitProvider()
+        {
+            if (provider is null)
+            {
+                InitializeProvider();
+            }
+
+            return provider;
+        }
+
+        private static void InitializeProvider()
+            => provider = new ServiceCollection()
+            .AddSingleton<IDisukuBotClient, DisukuBotClient>()
+            .BuildServiceProvider();
+    }
+}
---
+++ DisukuConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using DisukuBot.DisukuCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace DisukuConsoleApp
+{
+    class Program
+    {        
+        static async Task Main(string[] args)
+        {
+            await ActivatorUtilities.CreateInstance<BotClient>(InversionOfControl.Provider).RunAsync();
+        }
+    }
+}
