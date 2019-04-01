a3b3acc5b0226544f99f206baf1e60d32c5b01f1
Joel Parkinson <joelparkinson@live.co.uk>
2019-04-01 19:44:48 +0100

    Update DisukuEntityConverter.cs

--- DisukuBot/DisukuDiscord/Converters/DisukuEntityConverter.cs
+++ DisukuBot/DisukuDiscord/Converters/DisukuEntityConverter.cs
@@ -1,6 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using DisukuBot.DisukuCore.Entities.Embeds;
 using DisukuBot.DisukuCore.Entities.Logging;
 
 namespace DisukuBot.DisukuDiscord.Converters
