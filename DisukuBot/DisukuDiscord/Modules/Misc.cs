cdf0818b845e3f1ff8ebc4e707538f9cd4e9fc3a
Joel Parkinson <joelparkinson@live.co.uk>
2019-04-03 00:37:28 +0100

    Add Start of Misc Commands Section

---
+++ DisukuBot/DisukuDiscord/Modules/Misc.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisukuBot.DisukuDiscord.Modules
+{
+    [Name("Misc Commands")]
+    public class Misc : ModuleBase<SocketCommandContext>
+    {
+        [Command("Choose"), Summary("Selects between options given split by a comma.")]
+        public async Task Choose([Remainder]string message)
+        {
+            if (!message.Contains(','))
+            {
+                await ReplyAsync("You don't seem to have given me enough options.");
+                return;
+            }
+            var options = message.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            var rand = new Random();
+            var randNum = rand.Next(0, options.Length);
+            await ReplyAsync($"Hmm, I choose: `{options[randNum]}`");
+        }
+    }
+}
