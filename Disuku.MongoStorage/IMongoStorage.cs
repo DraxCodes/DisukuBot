1a4cd1de13fd9a8d016fe52cd223d835973cc68b
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-16 21:58:22 +0100

    Setup For MongoDb

---
+++ Disuku.MongoStorage/IMongoStorage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Disuku.MongoStorage
+{
+    public interface IMongoDbStorage
+    {
+        void LoadDataBase(string database);
+        bool Exists();
+        void Store<T>(T Item);
+        T RestoreSingle<T>(T Item);
+        IEnumerable<T> RestoreMany<T>(T item);
+    }
+}
---
+++ Disuku.MongoStorage/MongoStorage.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Disuku.MongoStorage
+{
+    public class MongoDbStorage : IMongoDbStorage
+    {
+        private IMongoDatabase db;
+
+        public void LoadDataBase(string database)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(database);
+        }
+
+        public bool Exists()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerable<T> RestoreMany<T>(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RestoreSingle<T>(T Item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store<T>(T Item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
