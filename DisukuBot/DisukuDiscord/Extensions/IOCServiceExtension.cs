75940070992463068fcdd20534b53df48129a5fa
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-18 01:52:00 +0100

    Update IOCServiceExtension.cs

--- DisukuBot/DisukuDiscord/Extensions/IOCServiceExtension.cs
+++ DisukuBot/DisukuDiscord/Extensions/IOCServiceExtension.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Disuku.Discord.DisukuDiscord.Extensions
