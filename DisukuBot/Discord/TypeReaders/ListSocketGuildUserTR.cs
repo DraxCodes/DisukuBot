9539ca9d2b9d1fcc77fb5eb6347a662340808745
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-18 02:31:01 +0100

    Update ListSocketGuildUserTR.cs

--- DisukuBot/Discord/TypeReaders/ListSocketGuildUserTR.cs
+++ DisukuBot/Discord/TypeReaders/ListSocketGuildUserTR.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DisukuBot.DisukuDiscord.TypeReaders
+namespace Disuku.Discord.TypeReaders
 {
     public class ListSocketGuildUserTR : TypeReader
     {
