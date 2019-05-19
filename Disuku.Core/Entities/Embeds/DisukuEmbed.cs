7f09f51bc9b0aa08057e6324ff45110762f0e9ce
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-20 00:03:27 +0100

    Add DisukuEmbed discord message support.

--- Disuku.Core/Entities/Embeds/DisukuEmbed.cs
+++ Disuku.Core/Entities/Embeds/DisukuEmbed.cs
@@ -8,6 +8,7 @@ namespace Disuku.Core.Entities.Embeds
         public string Description { get; set; }
         public string URL { get; set; }
         public string Thumbnail { get; set; }
+        public string ImageUrl { get; set; }
         public string Footer { get; set; }
         public List<Field> Fields { get; set; } = new List<Field>();
     }
