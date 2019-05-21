f069fca735746ad361e4c60287f563468dbc14d1
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-21 02:27:29 +0100

    Implement DisukuGuild Profile Provider
    Previous commit was Service.

--- Disuku.Core/Providers/Profile/DisukuGuildProvider.cs
+++ Disuku.Core/Providers/Profile/DisukuGuildProvider.cs
@@ -1,6 +1,26 @@
-﻿namespace Disuku.Core.Providers.Profile
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Disuku.Core.Entities;
+using Disuku.Core.Storage;
+
+namespace Disuku.Core.Providers.Profile
 {
     public class DisukuGuildProvider
     {
+        private IMongoDbStorage _mongoDbStorage;
+
+        public DisukuGuildProvider(IMongoDbStorage mongoDbStorage)
+        {
+            _mongoDbStorage = mongoDbStorage;
+            _mongoDbStorage.InitializeDbAsync("DisukuBot");
+        }
+
+        public async Task<DisukuGuild> GetGuild(DisukuGuild guild)
+        {
+            await _mongoDbStorage.UpsertSingleRecordAsync("Guilds", guild.Id, guild);
+            var results = await _mongoDbStorage.LoadRecordsAsync<DisukuGuild>("Guilds", u => u.GuildId == guild.GuildId);
+            return results.FirstOrDefault();
+        }
     }
 }
