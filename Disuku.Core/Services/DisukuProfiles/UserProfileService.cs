b8496600f2a321ed1d19520fb6d22ef9d29c7668
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-21 01:17:49 +0100

    Implement UserProfile Service
    Service currently sends a users seeded Guid only.
    Fetches/Updates User Profiles from the UserProvider

--- Disuku.Core/Services/DisukuProfiles/UserProfileService.cs
+++ Disuku.Core/Services/DisukuProfiles/UserProfileService.cs
@@ -1,6 +1,24 @@
-﻿namespace Disuku.Core.Services.DisukuProfiles
+﻿using Disuku.Core.Discord;
+using Disuku.Core.Entities;
+using Disuku.Core.Providers.Profile;
+using System.Threading.Tasks;
+
+namespace Disuku.Core.Services.DisukuProfiles
 {
     public class UserProfileService
     {
+        private DisukuUserProvider _userProvider;
+        private IDiscordMessage _discordMessage;
+        public UserProfileService(DisukuUserProvider userProvider, IDiscordMessage discordMessage)
+        {
+            _userProvider = userProvider;
+            _discordMessage = discordMessage;
+        }
+
+        public async Task ReplyUserAsync(ulong chanId, DisukuUser user)
+        {
+            var disukuUser = await _userProvider.GetUser(user);
+            await _discordMessage.SendDiscordMessageAsync(chanId, disukuUser.Id.ToString());
+        }
     }
 }
