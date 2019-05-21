99b0946321d55125796bc3f91304e0fc507da62b
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-21 01:41:59 +0100

    Implement DisukuUser DiscordMessage
    Add the ability to send a message to discord through the IDiscordMessage abstraction that contains a DisukuUser.
    
    (Overload for SendDiscordMessageAsync)

--- Disuku.Discord/Discord/Adapters/DiscordMessage.cs
+++ Disuku.Discord/Discord/Adapters/DiscordMessage.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 using Disuku.Core.Discord;
+using Disuku.Core.Entities;
 using Disuku.Core.Entities.Embeds;
 using Disuku.Discord.Converters;
 
@@ -19,19 +21,49 @@ namespace Disuku.Discord.Discord.Adapters
 
         public async Task SendDiscordMessageAsync(ulong chanId, string message)
         {
-            var channel = _client.GetChannel(chanId) as SocketTextChannel;
-            if(channel is null) { throw new InvalidCastException($"Unable to cast: {typeof(SocketChannel)} to {typeof(SocketTextChannel)} in {GetType().Name} " ); }
-
+            var channel = GetSocketTextChannel(chanId);
             await channel.SendMessageAsync(message);
+        }
+
+        public async Task SendDiscordMessageAsync(ulong chanId, DisukuUser user)
+        {
+            var formattedRoles = new StringBuilder();
+            foreach (var role in user.Roles)
+            {
+                if (role.Name == "@everyone") { formattedRoles.Append($"{role.Name}, "); continue; }
+                formattedRoles.Append($"<@&{role.Id}>, ");
+            }
+
+            var embed = new DisukuEmbed
+            {
+                Title = $"Profile: {user.Username}",
+                Description = 
+                $"**User ID:** {user.UserId}\n" +
+                $"**Guild ID:** {user.GuildId}\n" +
+                $"**Account Created:** {user.CreatedAt}\n" +
+                $"**Joined Server:** {user.JoinedAt}\n" +
+                $"**Roles:**\n" +
+                $"{formattedRoles}",
+                Thumbnail = user.AvatarUrl
+            };
+
+            await SendDiscordEmbedAsync(chanId, embed);
         }
 
         public async Task SendDiscordEmbedAsync(ulong chanId, DisukuEmbed embed)
         {
-            var channel = _client.GetChannel(chanId) as SocketTextChannel;
-            if (channel is null) { throw new InvalidCastException($"Unable to cast: {typeof(SocketChannel)} to {typeof(SocketTextChannel)} in {GetType().Name} "); }
+            var channel = GetSocketTextChannel(chanId);
 
             var discordEmbed = DisukuEntityConverter.ConvertEmbed(embed);
             await channel.SendMessageAsync(embed: discordEmbed);
         }
+
+        private SocketTextChannel GetSocketTextChannel(ulong chanId)
+        {
+            var channel = _client.GetChannel(chanId) as SocketTextChannel;
+            if (channel is null) { throw new InvalidCastException($"Unable to cast: {typeof(SocketChannel)} to {typeof(SocketTextChannel)} in {GetType().Name} "); }
+            return channel;
+        }
+
     }
 }
