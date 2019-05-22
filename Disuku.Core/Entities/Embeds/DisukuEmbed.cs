a1300950ca9587f56f36ca715a5de988fb1b1473
Joel Parkinson <joelparkinson@live.co.uk>
2019-05-22 03:27:54 +0100

    Update to Core 3.0

--- Disuku.Core/Entities/Embeds/DisukuEmbed.cs
+++ Disuku.Core/Entities/Embeds/DisukuEmbed.cs
@@ -7,10 +7,10 @@ namespace Disuku.Core.Entities.Embeds
         public string Title { get; set; }
         public string Description { get; set; }
         public string URL { get; set; }
-        public string Thumbnail { get; set; }
-        public string ImageUrl { get; set; }
-        public string Footer { get; set; }
-        public List<Field> Fields { get; set; } = new List<Field>();
+        public string? Thumbnail { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Footer { get; set; }
+        public List<Field>? Fields { get; set; } = new List<Field>();
     }
 
     public class Field
