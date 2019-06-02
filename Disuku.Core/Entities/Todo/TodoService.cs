5a678726c3e2526d1d8745c811591629ae6731e1
DraxCodes <joelparkinson@live.co.uk>
2019-06-02 21:14:16 +0200

    Add using system in TodoService

--- Disuku.Core/Entities/Todo/TodoService.cs
+++ Disuku.Core/Entities/Todo/TodoService.cs
@@ -1,25 +1,27 @@
+using System;
+
 namespace Disuku.Core.Services.Todo
 {
     public class TodoService : ITodoService
     {
         public void CompleteTodo(ulong userId, int id)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void CreateTodo(ulong userId, string details)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void DeleteTodo(ulong userId, int id)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public void ListTodo(ulong userId)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
