7390e86b7a94dd3ccd117c27576272b4aad99c74
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-14 20:59:45 +0100

    Change type of quote id
    Id type changed from ulong > guid.

--- Disuku.Core/Entities/Quotes/Quote.cs
+++ Disuku.Core/Entities/Quotes/Quote.cs
@@ -6,7 +6,7 @@ namespace Disuku.Core.Entities
 {
     public class Quote
     {
-        public ulong Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Message { get; set; }
         public string QuotedUserId { get; set; }
