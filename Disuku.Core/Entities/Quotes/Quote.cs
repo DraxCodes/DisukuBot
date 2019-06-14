951e84d94f355a9ff7ed6fcc14a7786b2e6b5a9e
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-14 18:38:26 +0100

    Implement Quote Entity

---
+++ Disuku.Core/Entities/Quotes/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Disuku.Core.Entities
+{
+    public class Quote
+    {
+        public ulong Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string QuotedUserId { get; set; }
+    }
+}
