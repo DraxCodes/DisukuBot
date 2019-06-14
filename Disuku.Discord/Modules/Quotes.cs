23635af6d518bd68bbbcb94b14f223fc20ffc0bd
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-14 18:38:36 +0100

    Setup Quote Module

---
+++ Disuku.Discord/Modules/Quotes.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Disuku.Core.Entities;
+using Disuku.Core.Services.Quotes;
+using Disuku.Discord.Converters;
+
+namespace Disuku.Discord.Modules
+{
+    [Group("Quote")]
+    public class Quotes : ModuleBase<SocketCommandContext>
+    {
+        private readonly IQuoteService _quoteService;
+        public Quotes(IQuoteService quoteService)
+        {
+            _quoteService = quoteService;
+        }
+
+        [Command]
+        public async Task QuoteDisplay(ulong quoteId)
+        {
+            await _quoteService.Find(Context.Channel.Id, quoteId);
+        }
+
+        [Command]
+        public async Task QuoteDisplay(string quoteName)
+        {
+             await _quoteService.Find(Context.Channel.Id, quoteName);
+        }
+
+        [Command("Add")]
+        public async Task QuoteAdd(ulong quoteId, string quoteName)
+        {
+            var message = await Context.Channel.GetMessageAsync(quoteId);
+            if (message is null)
+            {
+                await ReplyAsync("Message not found, are you in the same channel as the quote?");
+                return;
+            }
+
+            var quote = new Quote
+            {
+                Id = quoteId,
+                Name = quoteName,
+                Message = message.Content,
+                QuotedUserId = message.Author.Username
+            };
+
+            await _quoteService.Add(Context.Channel.Id, quote);
+        }
+
+        [Command("List")]
+        public async Task ListQuotes(SocketGuildUser user)
+        {
+            var disukuUser = DisukuEntityConverter.ConvertToDisukuUser(user);
+            await _quoteService.List(Context.Channel.Id, disukuUser);
+        }
+    }
+}
