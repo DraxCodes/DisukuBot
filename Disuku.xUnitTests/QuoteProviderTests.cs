60d2c52f0c846c40186bc63c4dba09b9a12e0b6a
Joel Parkinson <joelparkinson@live.co.uk>
2019-07-07 23:31:16 +0100

    Setup Test Cases

---
+++ Disuku.xUnitTests/QuoteProviderTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Disuku.Core.Providers.Quotes;
+using Disuku.Core.Storage;
+using Moq;
+using Xunit;
+
+namespace Disuku.xUnitTests
+{
+    public class QuoteProviderTests
+    {
+
+        private readonly Mock<IQuoteProvider> _mockedQuoteProvider;
+        private readonly Mock<IDataStore> _mockedDataStore;
+
+        public QuoteProviderTests()
+        {
+            _mockedDataStore = new Mock<IDataStore>();
+            _mockedQuoteProvider = new Mock<IQuoteProvider>();
+        }
+
+        [Fact]
+        public async Task GetQuotes_ShouldReturnCollectionOfQuotes()
+        {
+            
+        }
+    }
+}
