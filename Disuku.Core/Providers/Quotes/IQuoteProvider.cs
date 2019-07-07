fc73f0caceb4698c433eb31c764326c210fe6063
Joel Parkinson <joelparkinson@live.co.uk>
2019-07-07 22:42:53 +0100

    Implement QuoteProvider Interface

---
+++ Disuku.Core/Providers/Quotes/IQuoteProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Disuku.Core.Entities;
+
+namespace Disuku.Core.Providers.Quotes
+{
+    public interface IQuoteProvider
+    {
+        IEnumerable<Quote> GetQuotes(ulong messageId);
+        Quote GetQuote(ulong userId);
+    }
+}
