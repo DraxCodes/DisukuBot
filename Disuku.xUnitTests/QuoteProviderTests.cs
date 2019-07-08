8903d48e09e1283c844a9e266647b1386dbbead3
Joel Parkinson <joelparkinson@live.co.uk>
2019-07-08 08:45:27 +0100

    Implement a few QuoteProvider tests.

--- Disuku.xUnitTests/QuoteProviderTests.cs
+++ Disuku.xUnitTests/QuoteProviderTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Disuku.Core.Entities;
 using Disuku.Core.Providers.Quotes;
 using Disuku.Core.Storage;
 using Moq;
@@ -8,14 +10,45 @@ namespace Disuku.xUnitTests
 {
     public class QuoteProviderTests
     {
-
         private readonly Mock<IQuoteProvider> _mockedQuoteProvider;
         private readonly Mock<IDataStore> _mockedDataStore;
-
+        
         public QuoteProviderTests()
         {
             _mockedDataStore = new Mock<IDataStore>();
             _mockedQuoteProvider = new Mock<IQuoteProvider>();
         }
+
+        private List<Quote> MockedQuotes()
+            => new List<Quote>
+            {
+                new Quote
+                {
+                    MessageId = 123,
+                    Message = "Testing"
+                }
+            };
+
+        [Fact]
+        public async Task GetQuotes_ShouldReturnCollection()
+        {
+            var quotes = await _mockedQuoteProvider.Object.GetQuotes(It.IsAny<ulong>());
+            Assert.NotNull(quotes);
+        }
+
+        [Fact]
+        public async Task GetQuotes_ShouldReturnCollectionOfQuotes()
+        {
+            var expectedQuotes = MockedQuotes();
+
+            _mockedQuoteProvider.Setup(s =>
+                s.GetQuotes(1234UL))
+                .ReturnsAsync(expectedQuotes);
+
+            var quotes = await _mockedQuoteProvider.Object.GetQuotes(1234UL);
+
+            Assert.Equal(expectedQuotes, quotes);
+        }
+
     }
 }
